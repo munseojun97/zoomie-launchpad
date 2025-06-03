54b8e3b83d404ef59693fba39e49d02effc37bdf
Mun Seojun <tkfkd9464@gmail.com>
2025-06-03 18:18:10 +0900

    fix: use modern Link style for Next.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <h1 className="text-3xl font-bold">🚀 Zoomie Launchpad</h1>
       <p>당신의 밈코인을 프리세일로 세상에 공개하세요!</p>
       <div className="space-x-4">
-        <Link href="/register"><a className="text-blue-600 underline">코인 등록</a></Link>
-        <Link href="/projects"><a className="text-blue-600 underline">프리세일 보기</a></Link>
-        <Link href="/admin"><a className="text-blue-600 underline">관리자 대시보드</a></Link>
+        <Link href="/register" className="text-blue-600 underline">코인 등록</Link>
+        <Link href="/projects" className="text-blue-600 underline">프리세일 보기</Link>
+        <Link href="/admin" className="text-blue-600 underline">관리자 대시보드</Link>
       </div>
     </div>
   );
